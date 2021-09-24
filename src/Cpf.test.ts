0b4f4ac07eae949fa06f538d5a6a8912aaabb10d
Cezar Gabriel Heinen Zaleski <cezar.zaleski@gmail.com>
2021-09-24 17:42:03 -0300

    test: adicionar método estácio para criar cpf

--- src/Cpf.test.ts
+++ src/Cpf.test.ts
@@ -3,9 +3,21 @@ import { Cpf } from './Cpf';
 
 test('Should cpf is valid', () => {
   //given
-  const cpf = new Cpf()
+  const cpf = Cpf.create('010.233.120-00')
   //when
   expect(console.log('sim'))
+
+
+  //then
+
+})
+
+test('Should verify digit the cpf', () => {
+  //given
+  const cpf = Cpf.create('010.233.120-00')
+  //when
+  expect(console.log('sim'))
+
 
   //then
 
