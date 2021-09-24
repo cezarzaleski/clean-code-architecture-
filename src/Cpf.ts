6d57448ef558f38669db815860468314cf06fa2a
Cezar Gabriel Heinen Zaleski <cezar.zaleski@gmail.com>
2021-09-24 17:41:51 -0300

    feat: adicionar método estácio para criar cpf

--- src/Cpf.ts
+++ src/Cpf.ts
@@ -1,5 +1,17 @@
 
 
 export class Cpf {
+  private readonly cpf: number;
+
+  private constructor(cpf: number) {
+    this.cpf = cpf;
+  }
+
+
+  public static create(cpf: string) {
+    return new Cpf(+cpf.replace(/[^0-9]/g, ''));
+  }
+
+
 
 }
