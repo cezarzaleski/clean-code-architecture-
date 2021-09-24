6c01c7676513a23bcc1854699b428804f433ee2e
Cezar Gabriel Heinen Zaleski <cezar.zaleski@gmail.com>
2021-09-24 19:16:34 -0300

    feat:add validator second digit cpf

--- src/Cpf.ts
+++ src/Cpf.ts
@@ -5,6 +5,7 @@ export class Cpf {
 
   private constructor(cpf: string) {
     this.validateFirstDigit(cpf);
+    this.validateSecondDigit(cpf);
     this.cpf = cpf;
   }
 
@@ -25,6 +26,20 @@ export class Cpf {
     const restDivision = sum % 11;
     const calculateFirstDigit = restDivision < 2 ? 0 : 11 - restDivision;
     if (calculateFirstDigit != firstDigit) throw new Error("cpf invalid");
+  }
+
+  private validateSecondDigit(value: string) {
+    const base = value.substr(0,10);
+    const secondDigit = +value.substr(10,1);
+    let count = 11;
+    let sum = 0;
+    base.split('').forEach((num) => {
+      sum += +num * count
+      count--;
+    });
+    const restDivision = sum % 11;
+    const calculateSecondDigit = restDivision < 2 ? 0 : 11 - restDivision;
+    if (calculateSecondDigit != secondDigit) throw new Error("cpf invalid");
   }
 
   public static create(cpf: string) {
