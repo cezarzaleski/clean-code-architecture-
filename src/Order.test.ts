df3c719c5fef82321a3ee00777c09e8a74bb662c
Cezar Gabriel Heinen Zaleski <cezar.zaleski@gmail.com>
2021-09-24 18:12:12 -0300

    fix: remove console.log

--- src/Order.test.ts
+++ src/Order.test.ts
@@ -6,7 +6,6 @@ test('Should order with 3 itens', () => {
   const order = new Order('Test description', 12.8, 12)
   //when
   //then
-  console.log(order.amount)
   expect(order.description).toEqual('Test description')
   expect(order.price).toEqual(12.8)
   expect(order.amount).toEqual(12)
