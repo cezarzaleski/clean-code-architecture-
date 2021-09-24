1d1ff28366d310b9c3214b51d8aa0155fe766008
Cezar Gabriel Heinen Zaleski <cezar.zaleski@gmail.com>
2021-09-24 19:03:34 -0300

    feat: validate first digit cpf

--- src/Cpf.test.ts
+++ src/Cpf.test.ts
@@ -2,23 +2,22 @@ import { Cpf } from './Cpf';
 
 
 test('Should cpf is valid', () => {
-  //given
+  //given/when
   const cpf = Cpf.create('010.233.120-00')
-  //when
-  expect(console.log('sim'))
-
-
   //then
+  expect(cpf.value).toEqual('01023312000')
 
+})
+
+test('Should throw exception cpf invalid', () => {
+  // then
+  expect(() => Cpf.create('010.233.120-99')).toThrow(new Error("cpf invalid"));
 })
 
 test('Should verify digit the cpf', () => {
-  //given
-  const cpf = Cpf.create('010.233.120-00')
-  //when
-  expect(console.log('sim'))
-
-
-  //then
-
+  //given/when
+  const cpf = Cpf.create('111.444.777-35')
+  // then
+  expect(cpf.value).toEqual('11144477735')
 })
+
--- src/Cpf.ts
+++ src/Cpf.ts
@@ -1,17 +1,33 @@
 
 
 export class Cpf {
-  private readonly cpf: number;
+  private readonly cpf: string;
 
-  private constructor(cpf: number) {
+  private constructor(cpf: string) {
+    this.validateFirstDigit(cpf);
     this.cpf = cpf;
   }
 
-
-  public static create(cpf: string) {
-    return new Cpf(+cpf.replace(/[^0-9]/g, ''));
+  get value (): string {
+    return this.cpf
   }
 
+  // ruim pq não posso expor para testar =)
+  private validateFirstDigit(value: string) {
+    const base = value.substr(0,9);
+    const firstDigit = +value.substr(9,1);
+    let count = 10;
+    let sum = 0;
+    base.split('').forEach((num) => {
+      sum += +num * count
+      count--;
+    });
+    const restDivision = sum % 11;
+    const calculateFirstDigit = restDivision < 2 ? 0 : 11 - restDivision;
+    if (calculateFirstDigit != firstDigit) throw new Error("cpf invalid");
+  }
 
-
+  public static create(cpf: string) {
+    return new Cpf(cpf.replace(/[^0-9]/g, ''));
+  }
 }
