57417cb8a7a200956bdb2f0b292e46f6498cafe8
Cezar Gabriel Heinen Zaleski <cezar.zaleski@gmail.com>
2021-09-24 22:18:54 -0300

    test:add cpf in order

--- src/order.test.ts
+++ src/order.test.ts
@@ -1,11 +1,23 @@
 import { Order } from './order';
 import { Cpf } from './cpf';
+import { InvalidCpfError } from './invalid-cpf-error';
+
+
+test('Should throw exception cpf invalid', () => {
+  expect(() => new Order('Test description', 12.8, 12, '01023312099'))
+    .toThrow(new InvalidCpfError('01023312099'));
+})
+
+test('Should throw exception cpf invalid', () => {
+  expect(() => new Order('Test description', 12.8, 12, '01023312099'))
+    .toThrow(new InvalidCpfError('01023312099'));
+})
+
 
 
 test('Should order with 3 itens', () => {
-  //given
+  //given/when
   const order = new Order('Test description', 12.8, 12, '01023312000')
-  //when
   //then
   expect(order.description).toEqual('Test description')
   expect(order.price).toEqual(12.8)
