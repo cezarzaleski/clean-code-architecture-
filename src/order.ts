9414e4fa120aaa4e427ae5d577b158a86e634d9a
Cezar Gabriel Heinen Zaleski <cezar.zaleski@gmail.com>
2021-09-24 23:49:11 -0300

    refactor: remove get and setter in order

--- src/order.ts
+++ src/order.ts
@@ -2,40 +2,19 @@ import { Cpf } from './cpf';
 
 
 export class Order {
-  private readonly _description: string;
-  private readonly _price: number;
-  private readonly _amount: number;
-  private readonly _cpf: Cpf;
-  private readonly _coupon: number;
+  description: string;
+  price: number;
+  amount: number;
+  cpf: Cpf;
+  coupon: number;
 
 
   constructor(description: string, price: number, amount: number, cpf: string, coupon: number) {
-    this._description = description;
-    this._price = price;
-    this._amount = amount;
-    this._cpf = Cpf.create(cpf);
-    this._coupon = coupon;
-  }
-
-
-  get description(): string {
-    return this._description;
-  }
-
-  get price(): number {
-    return this._price;
-  }
-
-  get amount(): number {
-    return this._amount;
-  }
-
-  get cpf(): Cpf {
-    return this._cpf;
-  }
-
-  get coupon(): number {
-    return this._coupon;
+    this.description = description;
+    this.price = price;
+    this.amount = amount;
+    this.cpf = Cpf.create(cpf);
+    this.coupon = coupon;
   }
 
   get total(): number {
