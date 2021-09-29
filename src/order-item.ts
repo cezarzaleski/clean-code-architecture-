a795104ad0904cda2e5b76387f6a3486bcb6a11a
Cezar Gabriel Heinen Zaleski <cezar.zaleski@gmail.com>
2021-09-28 23:03:22 -0300

    chore: create script by coverage

---
+++ src/order-item.ts
@@ -0,0 +1,45 @@
+import { Cpf } from './cpf';
+
+
+export class Order {
+  private readonly _description: string;
+  private readonly _price: number;
+  private readonly _amount: number;
+  private readonly _cpf: Cpf;
+  private readonly _coupon: number;
+
+
+  constructor(description: string, price: number, amount: number, cpf: string, coupon: number) {
+    this._description = description;
+    this._price = price;
+    this._amount = amount;
+    this._cpf = Cpf.create(cpf);
+    this._coupon = coupon;
+  }
+
+
+  get description(): string {
+    return this._description;
+  }
+
+  get price(): number {
+    return this._price;
+  }
+
+  get amount(): number {
+    return this._amount;
+  }
+
+  get cpf(): Cpf {
+    return this._cpf;
+  }
+
+  get coupon(): number {
+    return this._coupon;
+  }
+
+  get total(): number {
+    const total = this.price * this.amount;
+    return total - (total*this.coupon);
+  }
+}
